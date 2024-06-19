4e93a042f8c8dced3e96aa3ba4e54ff9b72b2655
Pamela Murillo <pamelamurilloa@gmail.com>
2024-06-18 18:30:31 -0600

    28 - Turbo Boost With Factories
    I can now make any ammount of posts I want easily using factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,53 +17,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
 
-        User::truncate();
-        Post::truncate();
-        Category::truncate();
+        Post::factory(5)->create();
 
-        $user = User::factory()->create();
-
-        $personal = Category::create([
-            'name'=>'Personal',
-            'slug'=>'personal'
-        ]);
-
-        $family = Category::create([
-            'name'=>'Family',
-            'slug'=>'family'
-        ]);
-
-        
-        $work = Category::create([
-            'name'=>'Work',
-            'slug'=>'work'
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $personal->id,
-            'title' => 'My first post',
-            'slug' => 'my-first-post',
-            'excerpt'=> 'My first excerpt',
-            'body' => '<p>My first body</p>'
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $family->id,
-            'title' => 'My second post',
-            'slug' => 'my-second-post',
-            'excerpt'=> 'My second excerpt',
-            'body' => '<p>My second body</p>'
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $work->id,
-            'title' => 'My third post',
-            'slug' => 'my-third-post',
-            'excerpt'=> 'My third excerpt',
-            'body' => '<p>My third body</p>'
-        ]);
     }
 }
