24be28385bdc7be3ec86db36518d331818a5a708
Pamela Murillo <pamelamurilloa@gmail.com>
2024-07-15 09:38:10 +0100

    61 - Toy chests and constracts
    As the man said, this is a bit difficult to swallow, more so with only one example. Still, I managed tog rasp a few things and was able to correctly identify how to make an interface and its 'user'

--- app/Http/Controllers/NewsletterController.php
+++ app/Http/Controllers/NewsletterController.php
@@ -2,9 +2,24 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illusminate\Http\Request;
+use App\Services\Newsletter;
 
 class NewsletterController extends Controller
 {
-    //
+    public function __invoke(Newsletter $newsletter)
+    {
+        request()->validate(['email' => 'required|email']);
+
+        try {
+            $newsletter->subscribe(request('email'));
+        } catch (Exception $e) {
+            throw ValidationException::withMessages([
+                'email' => 'This email could not be added to our newsletter list.'
+            ]);
+        }
+
+        return redirect('/')
+            ->with('success', 'You are now signed up for our newsletter!');
+    }
 }
